7a11e5a6198ff60838bac90234589fcf02bb68b9
Luthfy Chaerudy <luthfy.chaerudy218@gmail.com>
2022-03-23 20:52:42 +0700

    seeding

---
+++ migrations/20220323133040-create-wallet.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable('Wallets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      accountBalance: {
+        type: Sequelize.INTEGER
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Wallets');
+  }
+};
